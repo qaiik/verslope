e61be2705f8c68051e46f52219f224ec2f83fc30
qaiik <selduaa2@gmail.com>
2022-11-30 20:39:24 -0700

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const app = require("express")();
 const path = require("path")
 
-app.get("/", (req,res) => {
-	res.sendFile(__dirname + "/slope" + "/index.html")
-})
 
 app.get("*", (req,res) => {
   res.sendFile(path.join(__dirname, "slope", req.path))
